6323fd014bf0cc5b5f62ee5220cf8a500b08f8cf
Michaël Van Meerbeek <michael.van.meerbeek@student.ehb.be>
2018-09-16 15:46:55 +0200

    Added simple websocket files

---
+++ src/main/java/com/chat/chat/controller/WebSocketController.java
@@ -0,0 +1,21 @@
+package com.chat.chat.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class WebSocketController {
+    private final SimpMessagingTemplate template;
+
+    @Autowired
+    public WebSocketController(SimpMessagingTemplate template) {
+        this.template = template;
+    }
+
+    @MessageMapping("/send/message")
+    public void onReceiveMessage(String message) {
+        template.convertAndSend("/chat", message);
+    }
+}
